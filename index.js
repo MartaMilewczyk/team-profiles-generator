53681a6283cb4f47d993e7485d1907310c1fa851
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-03-11 13:36:44 +0100

    createTeamManager function added

--- index.js
+++ index.js
@@ -12,3 +12,42 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
+let newTeam = [];
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+// create Team manager
+
+function teamManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter Manager's name:",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter Manager's ID:",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter Manager's email:",
+        },
+        {
+            type: 'input',
+            name: 'number',
+            message: "Please enter Manager's office number:",
+        }]).then((userInput) => {
+            const manager = new Manager(
+                userInput.name, 
+                userInput.id, 
+                userInput.email, 
+                userInput.number);
+            newTeam.push(manager);
+        });
+};
+
+
+
