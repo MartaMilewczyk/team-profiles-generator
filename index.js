0af585a0cb52cb2f0556141194d50e39659f6aa9
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-03-11 13:17:25 +0100

    Jest test passed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,9 +10,5 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
 
 
