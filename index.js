5864bc5faf2d7f86fb039fa6d8b915a71c767201
Marta Milewczyk <96419859+MartaMilewczyk@users.noreply.github.com>
2023-03-13 14:41:39 +0100

    createNewEmployee() and generateHTML() functions added

--- index.js
+++ index.js
@@ -11,14 +11,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 let newTeam = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
 // create Team manager
-
-function teamManager() {
+function createTeamManager() {
     inquirer.prompt([
         {
             type: 'input',
@@ -46,8 +42,80 @@ function teamManager() {
                 userInput.email, 
                 userInput.number);
             newTeam.push(manager);
+            console.log(newTeam);
+            createNewEmployee();
         });
 };
 
+// create new Employee
+function createNewEmployee() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'occupation',
+            message: "Please select occupation for new Employee:",
+            choices: ["Engineer", "Intern"],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: `Please enter Employee's name:`,
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Please enter Employee's ID:`,
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Please enter Employee's email:`,
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter Employee's school name:",
+            when: input => input.occupation === "Intern",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter Employee's github profile:",
+            when: input => input.occupation === "Engineer",
+        },
+        {
+            type: 'confirm',
+            name: 'confirmChoice',
+            message: 'Please confirm to add new Employee',
+            default: false,
+        }
+    ]).then((employeeInput) => {
+        const {name, id, email, occupation, github, school} = employeeInput;
 
+        if (occupation === "Engineer") {
+            newEmployee = new Engineer(name, id, email, github);
+        } else if (occupation === "Intern") {
+            newEmployee = new Intern(name, id, email, school)
+        }
+        newTeam.push(newEmployee);
+
+        createNewEmployee(newTeam) ? employeeInput.confirmChoice : generateHTML();
+    });
+};
+
+// function to create Team board in HTML
+function generateHTML() {
+    fs.mkdirSync(OUTPUT_DIR);
+    const template = render(newTeam);
+    fs.writeFile(outputPath, template, (error) => 
+    error ? console.error(error) : console.log("TEAM has been created succesfully!"));
+};
+
+// function to initialize program
+function init() {
+    createTeamManager();
+};
+
+// function call to initialize program
+init();
 
